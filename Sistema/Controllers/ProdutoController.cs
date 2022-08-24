1de7cf0c92a1b404f4a2aafa14039d75f057b684
Diego Melo <di.sinfo35@gmail.com>
2022-08-24 18:53:27 -0300

    Trabalhando erros ao buscar produto

--- Sistema/Controllers/ProdutoController.cs
+++ Sistema/Controllers/ProdutoController.cs
@@ -3,6 +3,7 @@ using Sistema.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -19,9 +20,20 @@ namespace Sistema.Controllers
         }
 
         // GET: Produto/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Details(int? id)
         {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
             var produto = db.Produto.Find(id);
+
+            if(produto == null)
+            {
+                return HttpNotFound();
+            }
+
             return View(produto);
         }
 
